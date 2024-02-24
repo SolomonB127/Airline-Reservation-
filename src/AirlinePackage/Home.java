add85b816d6290f80d51df318bb031ab2a0bd470
SolomonB127 <solomonakinsulire@gmail.com>
2024-02-24 17:39:33 +0000

    Created the frame for Home page

--- src/AirlinePackage/Home.java
+++ src/AirlinePackage/Home.java
@@ -1,4 +1,48 @@
 package AirlinePackage;
 
-public class Home {
+//Importation of utilities
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import javax.imageio.*;
+import java.awt.*;
+import java.sql.*;
+public class Home extends JFrame {
+
+    public Home() {
+//        Production of frame
+        setUndecorated(true);
+        setLayout(null);
+        Image image = null;
+        try {
+            image = ImageIO.read(new File("src/AirlinePackage/9ce85e495a62999592932927729323bc.jpg"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+//        Project Logo
+        ImageIcon img = new ImageIcon("src/AirlinePackage/Logo1.png");
+        JLabel imgLabel = new JLabel();
+        imgLabel.setSize(300, 300);
+        imgLabel.setLocation(100, 100);
+        imgLabel.setHorizontalAlignment(JLabel.CENTER);
+        imgLabel.setVerticalAlignment(JLabel.TOP);
+        imgLabel.setIcon(img);
+        add(imgLabel);
+
+
+        //        Jlabel for frame
+        JLabel imglabel = new JLabel(new ImageIcon(image));
+        imglabel.setSize(500,800);
+        imglabel.setLocation(0, 0);
+
+        add(imglabel);
+        setSize(500,750);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new Home(); //method call
+    }
 }
