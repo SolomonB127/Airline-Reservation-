b8403b4439d847b43aab56251528f352cb03dda3
SolomonB127 <solomonakinsulire@gmail.com>
2024-02-24 17:52:50 +0000

    Received the Username parameter from the Login page to the Home page, so-as to display a greeting message to logged-in user.

--- src/AirlinePackage/Home.java
+++ src/AirlinePackage/Home.java
@@ -1,6 +1,6 @@
 package AirlinePackage;
 
-//Importation of utilities
+// Importation of utilities
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -8,10 +8,10 @@ import java.io.*;
 import javax.imageio.*;
 import java.awt.*;
 import java.sql.*;
-public class Home extends JFrame {
 
-    public Home() {
-//        Production of frame
+public class Home extends JFrame {
+    public Home(String username) {
+        // Production of frame
         setUndecorated(true);
         setLayout(null);
         Image image = null;
@@ -20,7 +20,8 @@ public class Home extends JFrame {
         } catch (IOException e) {
             e.printStackTrace();
         }
-//        Project Logo
+
+        // Project Logo
         ImageIcon img = new ImageIcon("src/AirlinePackage/Logo1.png");
         JLabel imgLabel = new JLabel();
         imgLabel.setSize(300, 300);
@@ -30,19 +31,21 @@ public class Home extends JFrame {
         imgLabel.setIcon(img);
         add(imgLabel);
 
+        // Display welcome message
+        JLabel welcomeLabel = new JLabel("Hi, " + username + ". Welcome");
+        welcomeLabel.setSize(300, 50); // Set appropriate size
+        welcomeLabel.setLocation(100, 400); // Set appropriate location
+        add(welcomeLabel);
 
-        //        Jlabel for frame
+        // Jlabel for frame
         JLabel imglabel = new JLabel(new ImageIcon(image));
         imglabel.setSize(500,800);
         imglabel.setLocation(0, 0);
-
         add(imglabel);
+
         setSize(500,750);
         setLocationRelativeTo(null);
         setVisible(true);
     }
 
-    public static void main(String[] args) {
-        new Home(); //method call
-    }
 }
