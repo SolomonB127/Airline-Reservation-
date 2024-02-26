b0f127ea675dfb33198ac2eeb499ca0acf95d1c9
SolomonB127 <solomonakinsulire@gmail.com>
2024-02-26 13:02:59 +0000

    Added tables with list of flights available to Home frame

--- src/AirlinePackage/Home.java
+++ src/AirlinePackage/Home.java
@@ -4,12 +4,16 @@ package AirlinePackage;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.io.*;
 import javax.imageio.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 import java.sql.*;
 
 public class Home extends JFrame {
+    private JTable table;
     public Home(String username) {
         // Production of frame
         setUndecorated(true);
@@ -20,6 +24,8 @@ public class Home extends JFrame {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        Font fnt = new Font("Garamond", Font.BOLD, 15);
 
         // Project Logo
         ImageIcon img = new ImageIcon("src/AirlinePackage/Logo1.png");
@@ -32,10 +38,43 @@ public class Home extends JFrame {
         add(imgLabel);
 
         // Display welcome message
-        JLabel welcomeLabel = new JLabel("Hi, " + username + ". Welcome");
+        JLabel welcomeLabel = new JLabel("Welcome, " + username + ".");
         welcomeLabel.setSize(300, 50); // Set appropriate size
-        welcomeLabel.setLocation(100, 400); // Set appropriate location
+        welcomeLabel.setLocation(100, 250); // Set appropriate location
+        welcomeLabel.setFont(fnt);
         add(welcomeLabel);
+
+        // Column Names
+        String[] columnNames = {"Airline", "Destination", "Time", "Available Seats"};
+
+        // Flights-Data
+        Object[][] data = {
+                {"Airline A", "City B", "10:00", 5},
+                {"Airline C", "City D", "15:00", 0},
+                {"Airline E", "City F", "20:00", 3}
+        };
+        // Create table
+        table = new JTable(new DefaultTableModel(data, columnNames));
+
+        // Add mouse listener to handle row clicks
+        table.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+                int row = table.rowAtPoint(e.getPoint());
+                int col = table.columnAtPoint(e.getPoint());
+                if (col == 3 && (int)table.getValueAt(row, col) > 0) {
+                    // If the user clicked on a row with available seats, navigate to booking page
+                    // TODO: Implement navigation to booking page
+                    System.out.println("Navigating to booking page for " + table.getValueAt(row, 0));
+                }
+            }
+        });
+
+        // Add table to frame
+        JScrollPane scrollPane = new JScrollPane(table);
+        scrollPane.setBounds(50, 300, 400, 400); // Set the bounds as per your requirement
+        this.add(scrollPane);
+
+
 
         // Jlabel for frame
         JLabel imglabel = new JLabel(new ImageIcon(image));
